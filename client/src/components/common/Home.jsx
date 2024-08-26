c552ac4dde5e4f325d4dc09ea47995a3276d306e
Iria Moreno Rodriguez <iria.moreno12@gmail.com>
2024-08-26 14:55:50 +0200

    updates

---
+++ client/src/components/common/Home.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
+
+const NavBar = () => {
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
+  return (
+    
+    <nav className="navbar">
+    <div className="menu-icon" onClick={handleClick}>
+      <i className={click ? "fas fa-times" : "fas fa-bars"} />
+    </div>
+    <div className='links'>
+      <ul className={click ? "nav-menu active" : "nav-menu"}>
+
+        <Link className="nav-link" to="/" onClick={closeMobileMenu}>
+          Home
+        </Link>
+
+        <Link className="nav-link" to="/profile" onClick={closeMobileMenu}>
+          Profile
+        </Link>
+
+    </ul>
+    </div>
+    
+  </nav>
+  )};
+export default NavBar
